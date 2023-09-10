a639f03cef7129d652ee317fe8cf909f8fc6b6c8
shimamura10 <kshimamura1717@gmail.com>
2023-09-10 15:08:36 +0900

    授業スレッド追加のバックエンド作成

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -17,4 +17,24 @@ class LessonController extends Controller
         
         return view('posts/index')->with(['lessons' => $lesson->getPaginateByLimit($lesson_id, $time_id, $teacher_id), 'times' => $time->get(), 'teachers' => $teacher->get()]);
     }
+    
+    public function create(Time $time)
+    {
+        return view('posts/create')->with(['times' => $time->get()]);
+    }
+    
+    public function store(Request $request, Teacher $teacher, Lesson $lesson)
+    {
+        $teacher->name = $request->input('teacher_name');
+        $teacher->save();
+        $lesson->teacher_id = $teacher->id;
+        $lesson->time_id = $request->input('time_id');
+        $lesson->name = $request->input('lesson_name');
+        $lesson->comments = 0;
+        $lesson->atmosphere_average = 0;
+        $lesson->task_amount_average = 0;
+        $lesson->save();
+        
+        return redirect('/');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/', [LessonController::class, 'index'])->name('index');
+    Route::get('/createLesson', [LessonController::class, 'create'])->name('create.lesson');
+    Route::post('/storeLesson', [LessonController::class, 'store'])->name('store.lesson');
 });
 
 require __DIR__.'/auth.php';
